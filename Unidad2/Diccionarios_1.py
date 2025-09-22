f9132573c3c13016af2f11787593dd97f3b3ea8a
efloresgallegos <eduardo.fg@pabellon.tecnm.mx>
2025-09-22 07:47:10 -0600

    Diccionarios2

--- Unidad2/Diccionarios_1.py
+++ Unidad2/Diccionarios_1.py
@@ -1,21 +1,22 @@
-edades = {
-    "Brayan": 25,"Pabellón de Arteaga",
+Edades = {
+    "Brayan": 25,
     "Luis": 30,
     "José": 22
 }
-print("Edad de Luis:", edades["Luis"])    
-edades["Pedro"] = 28
-print("\nDespués de añadir a Pedro:")
+print("Edad de Luis:", edades["Luis"])   
+
+Edades["Brayan"] = 28
+print("\nDespués de añadir a Brayan:")
 print(edades)                               
 
-edades["Luis"] = 26
+Edades["Luis"] = 26
 print("\nDespués de actualizar la edad de Luis:")
 print(edades)                              
 
-del edades["María"]
+del Edades["José"]
 print("\nDespués de eliminar a José:")
-print(edades)                               
+print(Edades)                               
 
 print("\nRecorriendo el diccionario:")
-for nombre, edad in edades.items():         
-     print(f"{nombre} tiene {edad} años")
+for Nombre, Edad in Edades.items():         
+     print(f"{Nombre} tiene {Edad} años")
